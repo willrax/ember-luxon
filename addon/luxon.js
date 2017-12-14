7827987bdc6723b89bbcb4a9ed9f9e385a049795
Will Raxworthy <me@willrax.com>
2017-12-14 11:44:48 +0000

    deprecate the use of ember-luxon import

---
+++ addon/luxon.js
@@ -0,0 +1,6 @@
+import { DateTime, Duration, Info, Interval, Settings, Zone } from 'luxon';
+import { deprecate } from '@ember/application/deprecations';
+
+deprecate('The name of the import has been shortened from ember-luxon/luxon to just luxon. The old import will be removed in the next major version.', false, { id: 'ember-luxon/luxon-import', until: '1.1.0' });
+
+export { DateTime, Duration, Info, Interval, Settings, Zone };

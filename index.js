ed785bb41f7b8180a315731f1fb96e2fe9f56489
Will Raxworthy <me@willrax.com>
2018-01-19 16:20:21 +0000

    swap ember-rollup for custom implementation

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 /* eslint-env node */
 'use strict';
 
-const emberRollup = require('ember-rollup');
-const runtimeDependencies = [{
-  name: 'luxon',
-  namespaced: false
-}];
+const Rollup = require('broccoli-rollup');
+const { es6Transform } = require('ember-cli-es6-transform');
+const mergeTrees = require('broccoli-merge-trees');
 
-module.exports = emberRollup(runtimeDependencies, {
-  name: 'ember-luxon'
-});
+module.exports = {
+  name: 'ember-luxon',
+
+  included(app) {
+    this._super.included.apply(app, arguments);
+
+    app.import('vendor/luxon.js');
+  },
+
+  treeForVendor(tree) {
+    let rollupTree = new Rollup('./node_modules/luxon/src', {
+      rollup: {
+        input: 'luxon.js',
+        output: {
+          file: 'luxon.js',
+          format: 'es',
+        }
+      }
+    });
+
+    const babel = this.parent.findAddonByName('ember-cli-babel');
+    const babelOptions = babel.buildBabelOptions();
+
+    let foo = es6Transform(rollupTree, babelOptions);
+    return mergeTrees([foo, tree]);
+  }
+};

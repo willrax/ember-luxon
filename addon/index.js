6d9e8334c0edf80f4c2927d4a6c1f58687252eb8
Jan Buschtöns <jan.buschtoens@clark.de>
2018-08-14 21:48:38 +0200

    refactor(build): use ember-auto-import

---
+++ addon/index.js
@@ -0,0 +1,4 @@
+import { DateTime, Duration, Info, Interval, Settings, Zone } from 'luxon';
+import { deprecate } from '@ember/application/deprecations';
+
+export { DateTime, Duration, Info, Interval, Settings, Zone };

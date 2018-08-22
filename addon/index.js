a87a9e838d99e1309c096e8e6284b140c024f429
willrax <me@willrax.com>
2018-08-22 11:43:22 -0700

    remove unused function

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,2 @@
 import { DateTime, Duration, Info, Interval, Settings, Zone } from 'luxon';
-import { deprecate } from '@ember/application/deprecations';
-
 export { DateTime, Duration, Info, Interval, Settings, Zone };
